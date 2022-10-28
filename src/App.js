b47153720364a791b38e88fc09d27ec31bdc4f6f
Theobano <divinedotun@gmail.com>
2022-10-28 23:39:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { NavBar } from "./components";
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Users = React.lazy(() => import("./pages/Users"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <NavBar />
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} end />
+          <Route path="/users" element={<Users />} end />
+          <Route path="*" element={<NotFound />} end/>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
